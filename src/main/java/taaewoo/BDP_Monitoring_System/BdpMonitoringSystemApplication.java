23e23a61d44d5517aba11656f1d226f64f6fee9d
Taewoo <kauasltw@gmail.com>
2023-02-02 17:14:25 +0900

    BDP Monitoring System Init

---
+++ src/main/java/taaewoo/BDP_Monitoring_System/BdpMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package taaewoo.BDP_Monitoring_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdpMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdpMonitoringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/taaewoo/BDP_Monitoring_System/BdpMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package taaewoo.BDP_Monitoring_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdpMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
